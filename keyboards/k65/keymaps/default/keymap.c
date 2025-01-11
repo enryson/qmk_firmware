c1ee845f6fc382d29ffa6b179000603b43fb06dd
Enryson Oliveira Ferraz <enryson@gmail.com>
2025-01-11 00:03:17 -0300

    feature: add bluetooth support

--- keyboards/k65/keymaps/default/keymap.c
+++ keyboards/k65/keymaps/default/keymap.c
@@ -22,8 +22,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Layer 1 - Funções e Multimídia
     [1] = LAYOUT_ortho_5x14(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,       KC_F4,        KC_F5,       KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,   KC_DEL,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,       KC_NO,        KC_NO,       KC_NO,   B_UP,     KC_MUTE, KC_VOLU, KC_NO,    KC_NO,    KC_NO,    QK_BOOT,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,       KC_NO,        KC_NO,       KC_NO,   B_DOWN,   KC_NO,   KC_VOLD, KC_NO,    KC_NO,    KC_NO,    KC_NO,
+        OU_BT,   KC_NO,   KC_NO,   KC_NO,       KC_NO,        KC_NO,       KC_NO,   B_UP,     KC_MUTE, KC_VOLU, KC_NO,    KC_NO,    KC_NO,    QK_BOOT,
+        OU_USB,   KC_NO,   KC_NO,   KC_NO,       KC_NO,        KC_NO,       KC_NO,   B_DOWN,   KC_NO,   KC_VOLD, KC_NO,    KC_NO,    KC_NO,    KC_NO,
         KC_NO,   KC_NO,   KC_NO,   RGB_TOGGLE,  RGB_EFFECT,   RGB_COLOR,   KC_NO,   KC_PSCR,  KC_NO,   KC_MPRV, KC_MPLY,  KC_MNXT,  KC_NO,    KC_NO,
         EE_CLR,   KC_NO,   KC_NO,   KC_NO,       KC_NO,        KC_NO,       KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO
     )
